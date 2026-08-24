b4a26cce8f2fee3244abfe1f2c71b741336b6835
LewisLint <lewislintlicker@gmail.com>
2026-08-24 13:12:15 -0400

    Refactor riplease.sh to use a function for IP leases
    Refactor script to define riplease function for IP lease renewal with hostname generation.

--- riplease.sh
+++ riplease.sh
@@ -1,27 +1,32 @@
 #!/bin/zsh
-cpl=0
-for ((i=1; i<=$1; i++)); do
-    ((cpl++))
-    sudo ip link set dev wlan0 down && \
-    sudo macchanger -r wlan0 && \
-    sudo ip link set dev wlan0 up 
+    riplease() {
+    cpl=0
+    for ((i=1; i<=$1; i++)); do
+        ((cpl++))
+        sudo ip link set dev wlan0 down && \
+        sudo macchanger -r wlan0 && \
+        sudo ip link set dev wlan0 up 
+    
+        HOSTNAME=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 12)
+    
+        # sudo dhclient -r wlan0 
+        # fuck the router. This script would be kinda fucking useless if we just 
+        # said "yo bro, im not using this anymore" when we're trying to exhaust the lease pool
+        sudo dhclient -H "$HOSTNAME" wlan0
+    
+        if sudo dhclient -H "$HOSTNAME" wlan0; then
+            echo "--> loop $cpl: Successfully claimed a new IP lease <---"
+        else
+            echo "--> loop $cpl: DHCP request failed <---"
+        fi
+        #sleep 60
+        # sleep is for the weak (people and wlan cards alike)
+    done
+    
+    # Keep in mind that this changes the interface identity and
+    # repeatedly requests DHCP leases.
+    # this might piss off the DHCP server lol
+}
 
-    HOSTNAME=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 12)
+pfck() { python -c "print('\f' * $1" | lp }
 
-    # sudo dhclient -r wlan0 
-    # fuck the router. This script would be kinda fucking useless if we just 
-    # said "yo bro, im not using this anymore" when we're trying to exhaust the lease pool
-    sudo dhclient -H "$HOSTNAME" wlan0
-
-    if sudo dhclient -H "$HOSTNAME" wlan0; then
-        echo "--> loop $cpl: Successfully claimed a new IP lease <---"
-    else
-        echo "--> loop $cpl: DHCP request failed <---"
-    fi
-    #sleep 60
-    # sleep is for the weak (people and wlan cards alike)
-done
-
-# Keep in mind that this changes the interface identity and
-# repeatedly requests DHCP leases.
-# this might piss off the DHCP server lol
