df123d11924ede39c317e0c1f3d38f7f2df0c080
LewisLint <lewislintlicker@gmail.com>
2026-08-24 09:15:51 -0400

    add dhcp-hostname randomization

--- idk.sh
+++ idk.sh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 
-while true;
-do 
-	sudo ip link set dev wlan0 down && \
-	sudo macchanger -r wlan0 && \
-	sudo ip link set dev wlan0 up && \
-	sudo dhclient wlan0
-	# sleep 60
+while true; do
+    sudo ip link set dev wlan0 down && \
+    sudo macchanger -r wlan0 && \
+    sudo ip link set dev wlan0 up
+
+    HOSTNAME=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 12)
+
+    sudo dhclient -r wlan0
+    sudo dhclient -H "$HOSTNAME" wlan0
+
+    sleep 60
 done
 
-# Keep in mind that this may or may not make your router run out of IP addresses
+# Keep in mind that this changes the interface identity and
+# repeatedly requests DHCP leases.
+# this might piss off the DHCP server lol
